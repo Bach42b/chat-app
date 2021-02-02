541688b838a082aa0f51b5c0235b7f192eb220e7
Bach42b <juli.a.julia.328.723@gmail.com>
2021-02-02 12:16:23 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_015026) do
+<<<<<<< HEAD
+ActiveRecord::Schema.define(version: 2021_02_02_015719) do
+
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+=======
+ActiveRecord::Schema.define(version: 2021_02_02_005033) do
+>>>>>>> ユーザー管理機能の実装
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,4 +44,9 @@ ActiveRecord::Schema.define(version: 2021_02_02_015026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+<<<<<<< HEAD
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
+=======
+>>>>>>> ユーザー管理機能の実装
 end
